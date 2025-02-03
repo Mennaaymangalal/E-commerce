16dfd6c4e96f2f696691ece1b8a9291a314e4328
Menna ayman <mennaaymangalal4@gmail.com>
2025-02-04 01:35:43 +0200

    update

---
+++ .history/src/Services/addProductWishlist_20250204000736.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+// export default async function addProductWishlist(productId) {
+//     const {data} = await axios.post("https://ecommerce.routemisr.com/api/v1/wishlist",{
+//             productId,
+//         },{
+//             headers:{
+//                 token:localStorage.getItem("token")
+//             }
+//         }) 
+//     console.log(data)
+//   }
+
+  function addProductWishlist(){
+    axios.post("https://ecommerce.routemisr.com/api/v1/wishlist") ,{
+         productId,
+             },{
+              headers:{
+                        token:localStorage.getItem("token")
+                    }
+                } 
+  }
